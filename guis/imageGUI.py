5afa3cc2d4ebe3339dbf9925e5e6bc8d691f8402
Roberta Eleanor Hunt <ngw861@science.domain>
2019-02-05 23:52:08 +0100

    Added file traverse, scroll category

--- guis/imageGUI.py
+++ guis/imageGUI.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QCheckBox, QApplication, QWidget, QLabel, QLineEdit, QPushButton, QComboBox, QPlainTextEdit, QMessageBox
+from PyQt5.QtWidgets import QCheckBox, QFileDialog, QApplication, QWidget, QLabel, QLineEdit, QPushButton, QComboBox, QPlainTextEdit, QMessageBox
 
 from guis.progressDialog import progressDialog
 from guis.basicGUI import basicGUI
@@ -26,17 +26,19 @@ class imageGUI(basicGUI):
         self.autoSwitch = True
         self.radius = 50
         self.columns = ['category','x','y']
+        self.log = pd.DataFrame(columns=self.columns)
         self.path = os.getcwd()
         self.imgsPath = os.path.join(self.path,'imgs')
         if not os.path.exists(self.imgsPath):
             os.mkdir(self.imgsPath)
-        self.img_formats = ['CR2','ARW','jpg','tif','tiff']
+        self.img_formats = ['CR2','arw','jpg','tif','tiff']
         self.img_fps = self.get_img_fps()
         self.img_fp = self.get_next_img_fp()
         self.img_format = self.img_fp.split('.')[-1]
         self.csv_fp = self.img_fp.replace(self.img_format,'csv')
         self.img = self.load_img(self.img_fp)
         self.canvas = self.img.copy()
+        self.progress = progressDialog()
         
         self.ORIG_HEIGHT, self.ORIG_WIDTH = self.img.shape[:2]
         self.csv_fps = glob(os.path.join(self.imgsPath,'*.csv'))
@@ -75,6 +77,11 @@ class imageGUI(basicGUI):
         self.toNextButton = QPushButton("Done, go to next butterfly")
         self.toNextButton.clicked.connect(self.next_butterfly)
         
+        self.toPrevButton = QPushButton("Back to previous butterfly")
+        self.toPrevButton.clicked.connect(self.prev_butterfly)
+        
+        self.toCustomButton = QPushButton("Choose butterfly")
+        self.toCustomButton.clicked.connect(self.selectFile)
         
         self.initUI()
         
@@ -85,14 +92,24 @@ class imageGUI(basicGUI):
         self.grid.addWidget(self.categoriesDropdown,1,1,1,2)
         self.grid.addWidget(self.rmCategoryButton,2,1,1,2)
         self.grid.addWidget(self.autoSwitchCategoryCheckbox,3,1,1,2)
-        self.grid.addWidget(self.logPreview,4,1,3,2)
-        self.grid.addWidget(self.logEditToggleButton,8,1,1,2)
-        self.grid.addWidget(self.logPreviewSaveButton,9,1,1,2)
+        self.grid.addWidget(self.logPreview,4,1,2,2)
+        self.grid.addWidget(self.logEditToggleButton,6,1,1,2)
+        self.grid.addWidget(self.logPreviewSaveButton,7,1,1,2)
+        self.grid.addWidget(self.toPrevButton,8,1,1,2)
+        self.grid.addWidget(self.toCustomButton,9,1,1,2)
         self.grid.addWidget(self.toNextButton,10,1,1,2)
-        
-
-        
         self.setLayout(self.grid)
+    
+    def keyPressEvent(self, event):
+        if type(event) == QtGui.QKeyEvent:
+            if event.key() == QtCore.Qt.Key_Alt:
+                self.next_butterfly()
+            
+    def wheelEvent(self,event):
+        amount = np.abs(event.angleDelta().y()//120)
+        direction = np.sign(event.angleDelta().y())
+        for i in range(amount):
+            self.switchCategory(direction=direction)
     
     def toggleAutoSwitchCategory(self):
         if self.autoSwitch == True:
@@ -101,14 +118,17 @@ class imageGUI(basicGUI):
             self.autoSwitch = True
     
     def next_butterfly(self):
-        msg = "Are you sure you want to move to the next one?"
-        reply = QMessageBox.question(self, 'Message', 
-                         msg, QMessageBox.Yes, QMessageBox.No)
+        #msg = "Are you sure you want to move to the next one?"
+        #reply = QMessageBox.question(self, 'Message', 
+        #                 msg, QMessageBox.Yes, QMessageBox.No)
     
-        if reply == QMessageBox.No:
-            return None
-        
-        self.save_log()
+        #if reply == QMessageBox.No:
+        #    return None
+        self.progress._open()
+        self.progress.update(30,'Loading Butterfly..')
+    
+        if len(self.log):
+            self.save_log()
         self.logPreview.clear()
         
         self.img_fps = self.get_img_fps()
@@ -120,10 +140,82 @@ class imageGUI(basicGUI):
         
         self.csv_fp = self.img_fp.replace(self.img_format,'csv')
         
-        self.log = pd.DataFrame(columns=self.columns)
-        self.log.sort_index(ascending=False).to_csv(self.csv_fp, header=False)
-        self.logPreview.setPlainText(open(self.csv_fp).read())
+        self.categoriesDropdown.setCurrentIndex(0)   
+        if os.path.exists(self.csv_fp):
+            self.log = pd.read_csv(self.csv_fp, names=self.columns)
+            self.redraw_circles(self.log)
+            self.logPreview.setPlainText(open(self.csv_fp).read())
+        else:
+            self.log = pd.DataFrame(columns=self.columns)
+            #self.log.sort_index(ascending=False).to_csv(self.csv_fp, header=False)
+            #self.logPreview.setPlainText(open(self.csv_fp).read())
+        #self.log.sort_index(ascending=False).to_csv(self.csv_fp, header=False)
+        #self.logPreview.setPlainText(open(self.csv_fp).read())
+        
+        self.progress._close()
+        
+    def prev_butterfly(self):
+        #msg = "Are you sure you want to move to the next one?"
+        #reply = QMessageBox.question(self, 'Message', 
+        #                 msg, QMessageBox.Yes, QMessageBox.No)
+    
+        #if reply == QMessageBox.No:
+        #    return None
+        self.progress._open()
+        self.progress.update(30,'Loading Butterfly..')
+        
+        if len(self.log):
+            self.save_log()
+        self.logPreview.clear()
+        
+        self.img_fps = self.get_img_fps()
+        self.img_fp = self.get_prev_img_fp()
+        self.img_format = self.img_fp.split('.')[-1]        
+        self.img = self.load_img(self.img_fp)
+        self.canvas = self.img.copy()
+        self.make_preview_img(self.canvas)
         self.categoriesDropdown.setCurrentIndex(0)
+        
+        self.csv_fp = self.img_fp.replace(self.img_format,'csv')
+        if os.path.exists(self.csv_fp):
+            self.log = pd.read_csv(self.csv_fp, names=self.columns)
+            self.redraw_circles(self.log)
+            self.logPreview.setPlainText(open(self.csv_fp).read())
+        else:
+            self.log = pd.DataFrame(columns=self.columns)
+            #self.log.sort_index(ascending=False).to_csv(self.csv_fp, header=False)
+            #self.logPreview.setPlainText(open(self.csv_fp).read())
+        
+        self.progress._close()
+    
+    def selectFile(self):
+        self.img_fps = self.get_img_fps()
+        self.img_fp = QFileDialog.getOpenFileName()[0]
+        self.progress._open()
+        self.progress.update(30,'Loading Butterfly..')
+        
+        if len(self.log):
+            self.save_log()
+        self.logPreview.clear()
+        
+        self.img_format = self.img_fp.split('.')[-1]        
+        self.img = self.load_img(self.img_fp)
+        self.canvas = self.img.copy()
+        self.make_preview_img(self.canvas)
+        self.categoriesDropdown.setCurrentIndex(0)
+        
+        self.csv_fp = self.img_fp.replace(self.img_format,'csv')
+        if os.path.exists(self.csv_fp):
+            self.log = pd.read_csv(self.csv_fp, names=self.columns)
+            self.redraw_circles(self.log)
+            self.logPreview.setPlainText(open(self.csv_fp).read())
+        else:
+            self.log = pd.DataFrame(columns=self.columns)
+            #self.log.sort_index(ascending=False).to_csv(self.csv_fp, header=False)
+            #self.logPreview.setPlainText(open(self.csv_fp).read())
+        
+        self.progress._close()
+        
     
     def toggle_log_edit(self):
         if self.logPreview.isEnabled():
@@ -188,7 +280,7 @@ class imageGUI(basicGUI):
         elif event.button() == QtCore.Qt.RightButton:
             nearby_circle = self.check_if_nearby_point(orig_x,orig_y)
             if nearby_circle == None:
-                self.autoSwitchCategory(self)
+                self.switchCategory()
             else:
                 self.log = self.log[self.log.index != nearby_circle]
                 self.update_log_file()
@@ -197,14 +289,20 @@ class imageGUI(basicGUI):
             self.warn('Button not understood.')
         
         if self.autoSwitch:
-            self.autoSwitchCategory()
+            self.switchCategory()
         
-    def autoSwitchCategory(self):
+    def switchCategory(self,direction=1):
         category_idx = self.categoriesDropdown.currentIndex()
-        if (category_idx + 1) == len(self.categories):
-            next_category_idx = 0
-        else:
-            next_category_idx = category_idx + 1
+        if direction >= 0:
+            if (category_idx + 1) == len(self.categories):
+                next_category_idx = 0
+            else:
+                next_category_idx = category_idx + 1
+        elif direction < 0:
+            if (category_idx - 1) < 0:
+                next_category_idx = (len(self.categories) - 1)
+            else:
+                next_category_idx = category_idx - 1
         self.categoriesDropdown.setCurrentIndex(next_category_idx)
         
     def update_log_file(self):
@@ -248,9 +346,9 @@ class imageGUI(basicGUI):
             img_fps += glob(os.path.join(self.imgsPath,'*.%s'%_format))
         if len(img_fps) == 0:
             self.warn('No images found in folder %s of types %s'%(self.imgsPath, self.img_formats))
-        return img_fps
+        return sorted(img_fps)
         
-    def get_next_img_fp(self):
+    def get_next_img_fp(self):  
         self.csv_fps = glob(os.path.join(self.imgsPath,'*.csv'))
         for img_fp in self.img_fps:
             img_format = img_fp.split('.')[-1]
@@ -258,6 +356,13 @@ class imageGUI(basicGUI):
             if csv_fp not in self.csv_fps:
                 return img_fp
         self.warn('No images without csv files found')
+        
+    
+    def get_prev_img_fp(self):
+        curr_index = self.img_fps.index(self.img_fp)
+        prev_index = max(0,curr_index-1)
+        return self.img_fps[prev_index]
+        
         
     def load_img(self, img_fp):
         img_format = img_fp.split('.')[-1]
